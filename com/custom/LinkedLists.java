8676e94e10f8aefbc6fbcc308db25c7616ca6d51
abhilashSreenivasa <abhilash.sreenivasa@gmail.com>
2020-06-25 22:38:03 +0530

    insert element,peek, Comments

--- com/custom/LinkedLists.java
+++ com/custom/LinkedLists.java
@@ -1,7 +1,9 @@
 package com.custom;
+// Singly Linked list consisting of only Integers.
 public class LinkedLists {
     public Node head;  
     public Node last;
+    public int size=0;
 
     class Node{
         public int val;
@@ -11,6 +13,8 @@ public class LinkedLists {
         }
     }
 
+
+    // Adds one element at a time to the end of the list.
     public void add(int val){
         Node node=new Node(val);
         if(head==null){
@@ -23,7 +27,11 @@ public class LinkedLists {
             }
             temp.next=node;
         }
+        ++size;
+        
     }
+
+    //Prints the value of each node of list to the console.
     public void printList(){
         Node temp=head;
         while(temp!=null){
@@ -31,15 +39,56 @@ public class LinkedLists {
             temp=temp.next;
         }
     }
-
+    
+    //Removes first match of the passed element value. Doesn't do anything if value is not present in the list
     public void removeEle(int ele){
        Node temp=head;
-       while(temp.next.val!=ele){
+       while(temp!=null && temp.next.val!=ele ){
            temp=temp.next;
        }
+       if(temp==null)
+       return;
        Node remove=temp.next;
        temp.next=temp.next.next;
         remove=null;
+        --size;
     }
+    
+    //Inserts element at any position starting from 0.
+    //Invalid position value will not do any operation
+    public void insert(int val,int pos){
+        Node node=new Node(val);
+        if(pos==0){
+            Node first=node;
+            first.next=head;
+            head=first;
+            return;
+            
+        }
+        int count=0;
+        if(head==null || pos<=count ||pos>size)
+        return;
+        
+        Node temp1=null;
+        while(count<pos){
+            if(temp1==null){
+                temp1=head;++count;
+            }
+            else{
+            temp1=temp1.next;
+            ++count;
+            }
+            
+        }
+        Node temp2=temp1.next;
+        temp1.next=node;
+        node.next=temp2;
+    }
+   
+    //Returns first Node.
+    public Node peek(){
+        return head;
+    }
+
 
 }
