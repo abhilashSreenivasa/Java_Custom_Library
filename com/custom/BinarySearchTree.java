5c74297938e8db729071a1077b31392e67fe89c1
abhilashSreenivasa <abhilash.sreenivasa@gmail.com>
2020-07-28 16:50:52 +0530

    Search and inorder traversal

--- com/custom/BinarySearchTree.java
+++ com/custom/BinarySearchTree.java
@@ -1,1 +1,81 @@
 package com.custom;
+public class BinarySearchTree{
+    public static Node root=null;
+    class Node{
+        public int val;
+        public Node left;
+        public Node right;
+        public Node(int val){
+            this.val=val;
+        }
+    }
+
+    public Object insert(int val){
+        Node node=new Node(val);
+        if(root==null){
+        root=node;
+        return root;
+        }
+        Node cur=root;
+        while(true){
+            if(val<cur.val){
+                if(cur.left==null){
+                cur.left=node;
+                return cur.left;
+                }
+                else
+                cur=cur.left;
+            }
+            if(val>cur.val){
+                if(cur.right==null){
+                    cur.right=node;
+                    return cur.right;
+                }
+                else
+                cur=cur.right;
+            }
+            if(val==cur.val)
+            return null;
+        }
+
+
+    }
+
+    public static boolean search(int val){
+        if(root==null)
+        return false;
+        Node temp=root;
+        while(true){
+            if(temp.val>val){
+                if(temp.left==null)
+                return false;
+                else
+                temp=temp.left;
+            }
+            else if(temp.val<val){
+                if(temp.right==null)
+                return false;
+                else
+                temp=temp.right;
+            }
+            else
+            return  true;
+        }
+
+
+    }
+
+    public static void printInOrder(){
+        inOrder(root);
+    }
+    protected static void inOrder(Node node){
+        if(node==null)
+        return;
+        inOrder(node.left);
+        System.out.println(node.val);
+        inOrder(node.right);
+    }
+
+    
+
+}
